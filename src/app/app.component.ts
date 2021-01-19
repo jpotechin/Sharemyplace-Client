85bd8d560efbf908abc2d978ddfae643a0a37d8f
jpotechin <josephpotechin@gmail.com>
2021-01-18 20:08:46 -0800

    Client Skeleton Created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
+interface Users {
+	id: number;
+	userName: string;
+}
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit {
+	title = 'Share My Place';
+	users: Users[] | [];
+
+	constructor(private http: HttpClient) {
+		this.users = [];
+	}
+
+	ngOnInit(): void {
+		this.getUsers();
+	}
+
+	getUsers(): void {
+		this.http.get('https://localhost:5001/api/users').subscribe(
+			(response: any) => {
+				this.users = response;
+			},
+			(error) => console.error(error)
+		);
+	}
 }
