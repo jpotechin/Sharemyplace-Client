f562a8b8d553c88394655640aff41968ac4f5437
jpotechin <josephpotechin@gmail.com>
2021-02-06 06:08:37 -0800

    Added env var and created a members service

--- src/app/interface/account.interface.ts
+++ src/app/interface/account.interface.ts
@@ -7,3 +7,25 @@ export interface SignInAndUp {
 	username: string;
 	password: string;
 }
+
+export interface Member {
+	UserName: string;
+	Gender: string;
+	DateOfBirth: string;
+	KnownAs: string;
+	Created: Date;
+	LastActive: Date;
+	Introduction: string;
+	LookingFor: string;
+	Interests: string;
+	City: string;
+	State: string;
+	OwnerRenter: boolean;
+	Photos: Photo[];
+}
+
+export interface Photo {
+	id: number;
+	Url: string;
+	IsMain: boolean;
+}
