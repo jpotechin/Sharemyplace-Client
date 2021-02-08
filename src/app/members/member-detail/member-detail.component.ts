ad629c93f437a1a3267b7a701184676c3b3c745f
jpotechin <josephpotechin@gmail.com>
2021-02-08 10:25:56 -0800

    Initial member details page and fixed member service

--- src/app/members/member-detail/member-detail.component.ts
+++ src/app/members/member-detail/member-detail.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { MembersService } from './../../_services/members.service';
+import { Member } from './../../interface/account.interface';
 
 @Component({
-  selector: 'app-member-detail',
-  templateUrl: './member-detail.component.html',
-  styleUrls: ['./member-detail.component.scss']
+	selector: 'app-member-detail',
+	templateUrl: './member-detail.component.html',
+	styleUrls: ['./member-detail.component.scss'],
 })
 export class MemberDetailComponent implements OnInit {
+	member!: Member;
 
-  constructor() { }
+	constructor(private memberService: MembersService, private route: ActivatedRoute) {}
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+		this.loadMember();
+	}
 
+	loadMember(): void {
+		// @ts-ignore
+		this.memberService.getMember(this.route.snapshot.paramMap.get('username')).subscribe((member) => {
+			this.member = member;
+		});
+	}
+	// loadMember(): void {
+	// 	const username = this.route.snapshot.paramMap.get('username');
+	// 	if (username) {
+	// 		this.memberService.getMember(username).subscribe((selectedMember) => {
+	// 			console.log('Member', selectedMember);
+	// 			this.member = selectedMember;
+	// 		});
+	// 	}
+	// }
 }
