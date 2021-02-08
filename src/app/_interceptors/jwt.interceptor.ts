483f5ffad38c66fc28b5fb48f076f809381fa72f
jpotechin <josephpotechin@gmail.com>
2021-02-08 06:27:10 -0800

    created a JWT interceptor for HTTP requests headers

---
+++ src/app/_interceptors/jwt.interceptor.ts
@@ -0,0 +1,29 @@
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { take } from 'rxjs/operators';
+import { User } from '../interface/account.interface';
+import { AccountService } from './../_services/account.service';
+
+@Injectable()
+export class JwtInterceptor implements HttpInterceptor {
+	constructor(private accountService: AccountService) {}
+
+	intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+		let currentUser: User | null;
+
+		this.accountService.currentUser$.pipe(take(1)).subscribe((user) => {
+			currentUser = user;
+			if (currentUser) {
+				request = request.clone({
+					setHeaders: {
+						// tslint:disable-next-line: no-unused-expression
+						Authorization: `Bearer ${currentUser.token}`,
+					},
+				});
+			}
+		});
+
+		return next.handle(request);
+	}
+}
