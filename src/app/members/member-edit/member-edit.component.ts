f287584f81bdcb0373c12bc430e7cfe2eacb8847
jpotechin <josephpotechin@gmail.com>
2021-02-21 16:11:23 -0800

    Setup layout for member edit component

--- src/app/members/member-edit/member-edit.component.ts
+++ src/app/members/member-edit/member-edit.component.ts
@@ -1,8 +1,11 @@
-import { MembersService } from './../../_services/members.service';
-import { AccountService } from './../../_services/account.service';
-import { Component, OnInit } from '@angular/core';
-import { Member, User } from 'src/app/interface/account.interface';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { NgxGalleryAnimation, NgxGalleryImage, NgxGalleryOptions } from '@kolkov/ngx-gallery';
+import { ToastrService } from 'ngx-toastr';
 import { take } from 'rxjs/operators';
+import { Member, User } from 'src/app/interface/account.interface';
+import { AccountService } from './../../_services/account.service';
+import { MembersService } from './../../_services/members.service';
 
 @Component({
 	selector: 'app-member-edit',
@@ -10,20 +13,68 @@ import { take } from 'rxjs/operators';
 	styleUrls: ['./member-edit.component.scss'],
 })
 export class MemberEditComponent implements OnInit {
+	@ViewChild('editForm') editForm!: NgForm;
 	member!: Member;
-	user!: User | null;
+	user: User | null = null;
+	openTab = 1;
+	galleryOptions: NgxGalleryOptions[] | undefined;
+	galleryImages: NgxGalleryImage[] | undefined;
 
-	constructor(private accountService: AccountService, private memberService: MembersService) {
+	constructor(
+		private accountService: AccountService,
+		private memberService: MembersService,
+		private toastr: ToastrService
+	) {
 		this.accountService.currentUser$.pipe(take(1)).subscribe((user) => (this.user = user));
 	}
 
 	ngOnInit(): void {
 		this.loadMember();
+		this.galleryOptions = [
+			{
+				width: '500px',
+				height: '500px',
+				imagePercent: 100,
+				thumbnailsColumns: 4,
+				imageAnimation: NgxGalleryAnimation.Slide,
+				preview: false,
+			},
+		];
 	}
 
 	loadMember(): void {
 		if (this.user) {
-			this.memberService.getMember(this.user.username).subscribe((member) => (this.member = member));
+			this.memberService.getMember(this.user.username).subscribe((member) => {
+				this.member = member;
+
+				this.galleryImages = this.getImages();
+			});
 		}
+	}
+
+	public toggleTabs($tabNumber: number): void {
+		this.openTab = $tabNumber;
+	}
+
+	getImages(): NgxGalleryImage[] {
+		const imageUrls = [];
+		console.log(this.member);
+		if (this.member?.photos) {
+			for (const photo of this.member?.photos) {
+				imageUrls.push({
+					small: photo?.url,
+					medium: photo?.url,
+					big: photo?.url,
+				});
+			}
+		}
+		console.log(imageUrls);
+		return imageUrls;
+	}
+
+	updateMember(): void {
+		console.log(this.member);
+		this.toastr.success('Profile updated successfully');
+		this.editForm.reset(this.member);
 	}
 }
