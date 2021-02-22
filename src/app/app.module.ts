59c5ac8cd8ed43ef4d377fd93fe7f7dbde8c43c8
jpotechin <josephpotechin@gmail.com>
2021-02-22 07:40:21 -0800

    Added loading to simulate loading in local env and fixed how data is loaded

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgxSpinnerModule } from 'ngx-spinner';
 import { ErrorInterceptor } from './_interceptors/error.interceptor';
 import { JwtInterceptor } from './_interceptors/jwt.interceptor';
+import { LoadingInterceptor } from './_interceptors/loading.interceptor';
 
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { SharedModule } from './_modules/shared.module';
@@ -18,11 +20,11 @@ import { HomeComponent } from './home/home.component';
 import { ListsComponent } from './lists/lists.component';
 import { MemberCardComponent } from './members/member-card/member-card.component';
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { MemberEditComponent } from './members/member-edit/member-edit.component';
 import { MemberListComponent } from './members/member-list/member-list.component';
 import { MessagesComponent } from './messages/messages.component';
 import { NavComponent } from './nav/nav.component';
 import { RegisterComponent } from './register/register.component';
-import { MemberEditComponent } from './members/member-edit/member-edit.component';
 @NgModule({
 	declarations: [
 		AppComponent,
@@ -45,10 +47,12 @@ import { MemberEditComponent } from './members/member-edit/member-edit.component
 		BrowserModule,
 		FormsModule,
 		HttpClientModule,
+		NgxSpinnerModule,
 		ReactiveFormsModule,
 		SharedModule,
 		FontAwesomeModule,
 	],
+	schemas: [CUSTOM_ELEMENTS_SCHEMA],
 	providers: [
 		{
 			provide: HTTP_INTERCEPTORS,
@@ -58,6 +62,11 @@ import { MemberEditComponent } from './members/member-edit/member-edit.component
 		{
 			provide: HTTP_INTERCEPTORS,
 			useClass: JwtInterceptor,
+			multi: true,
+		},
+		{
+			provide: HTTP_INTERCEPTORS,
+			useClass: LoadingInterceptor,
 			multi: true,
 		},
 	],
