f7902937077b426b7acbaafe4fa7ca392bc6cdde
jpotechin <josephpotechin@gmail.com>
2021-02-21 16:20:58 -0800

    Protected users from closing tab or going to another link with unsaved changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './_guards/auth.guard';
+import { PreventUnsavedChangesGuard } from './_guards/prevent-unsaved-changes.guard';
 
 import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { ServerErrorComponent } from './errors/server-error/server-error.component';
@@ -21,7 +22,7 @@ const routes: Routes = [
 		children: [
 			{ path: 'members', component: MemberListComponent },
 			{ path: 'members/:username', component: MemberDetailComponent },
-			{ path: 'member/edit', component: MemberEditComponent },
+			{ path: 'member/edit', component: MemberEditComponent, canDeactivate: [PreventUnsavedChangesGuard] },
 			{ path: 'lists', component: ListsComponent },
 			{ path: 'messages', component: MessagesComponent },
 		],
