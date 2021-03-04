2f8d59736a4655019f27b6dbc0e709d699679804
jpotechin <josephpotechin@gmail.com>
2021-03-03 16:43:09 -0800

    Added Message table

--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { faEnvelope, faEnvelopeOpen, faPaperPlane } from '@fortawesome/free-solid-svg-icons';
 import { MessageService } from './../_services/message.service';
 import { IMessage } from './../interface/message';
 import { IPagination } from './../interface/pagination';
+import { MembersService } from '../_services/members.service';
+import { UserParams } from '../interface/userParams';
 
 @Component({
 	selector: 'app-messages',
@@ -12,17 +15,25 @@ export class MessagesComponent implements OnInit {
 	messages!: IMessage[];
 	pagination: IPagination | undefined;
 	defaultMessage = 'No Messages';
-	container = 'Outbox';
+	container = 'Inbox';
 	pageNumber = 1;
 	pageSize = 5;
+	userParams: UserParams;
 
-	constructor(private messageService: MessageService) {}
+	unopenIcon = faEnvelope;
+	openIcon = faEnvelopeOpen;
+	outboxIcon = faPaperPlane;
+
+	constructor(private memberService: MembersService, private messageService: MessageService) {
+		this.userParams = this.memberService.getUserParams();
+	}
 
 	ngOnInit(): void {
 		this.loadMessages();
 	}
 
 	loadMessages(): void {
+		this.messages = [];
 		this.messageService.getMessages(this.pageNumber, this.pageSize, this.container).subscribe((response: any) => {
 			console.log(response);
 			this.messages = response.result;
@@ -33,6 +44,11 @@ export class MessagesComponent implements OnInit {
 
 	pageChanged(eventPage: any): void {
 		this.pageNumber = eventPage;
+		this.loadMessages();
+	}
+
+	selectButton(selection: string): void {
+		this.container = selection;
 		this.loadMessages();
 	}
 }
